59f49c502e2439f09d45f22e2f54386a8ce978fd
IamDarya <daria.vyacheslavovna@gmail.com>
2021-03-05 18:36:25 +0100

    init: fix Reverse Int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+    let arrayOfDigits = Array.from(String(n), Number);
+    let reversedArray = [];
+    let reversedArrayIndex = 0;
+    for (let i = arrayOfDigits.length - 1; i >= 0; i--) {
+            reversedArray[reversedArrayIndex] = arrayOfDigits[i];
+            reversedArrayIndex++;
+        }
+   let stringOfDigits = reversedArray.join('');
+    return Number(stringOfDigits);
 }
